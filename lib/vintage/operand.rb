7bd286c4b401702a165d18181eb7600005eb6c8e
Anthony Erlinger <aerlinger@gmail.com>
2014-01-01 23:24:36 -0500

    Add remaining addressing modes (Accumulator and Indirect)

--- lib/vintage/operand.rb
+++ lib/vintage/operand.rb
@@ -3,36 +3,42 @@ module Vintage
   module Operand
     def self.read(mem, mode, x, y)
       case mode
-      when "#" # Implicit 
-        nil
-      when "@" # Relative
-        offset = mem.next
+        when "A" # Accumulator
+          nil
+        when "#" # Implicit
+          nil
+        when "@" # Relative
+          offset = mem.next
 
-        mem.pc + (offset <= 0x80 ? offset : -(0xff - offset + 1)) 
-      when "IM" # Immediate
-        mem.pc.tap { mem.next }
-      when "ZP" # Zero Page
-        mem.next
-      when "ZX" # Zero Page, X
-        mem.next + x
-      when "ZY" # Zero Page, Y
-        mem.next + y
-      when  "AB" # Absolute
-        mem.int16([mem.next, mem.next])
-      when  "AX" # Absolute, Y
-        mem.int16([mem.next, mem.next]) + x
-      when  "AY" # Absolute, Y
-        mem.int16([mem.next, mem.next]) + y
-      when "IX" # Indexed Indirect
-        e = mem.next
+          mem.pc + (offset <= 0x80 ? offset : -(0xff - offset + 1))
+        when "IM" # Immediate
+          mem.pc.tap { mem.next }
+        when "ZP" # Zero Page
+          mem.next
+        when "ZX" # Zero Page, X
+          mem.next + x
+        when "ZY" # Zero Page, Y
+          mem.next + y
+        when "AB" # Absolute
+          mem.int16([mem.next, mem.next])
+        when "AX" # Absolute, Y
+          mem.int16([mem.next, mem.next]) + x
+        when "AY" # Absolute, Y
+          mem.int16([mem.next, mem.next]) + y
+        when "IN"
+          e = mem.next
+          mem.int16([mem[e], mem[e+1]])
+        when "IX" # Indexed Indirect
+          e = mem.next
 
-        mem.int16([mem[e + x], mem[e + x + 1]])
-      when "IY" # Indirect Indexed
-        e = mem.next
+          # Indexed indirect adds the X register with zero page wrap around:
+          mem.int16([mem[e + x], mem[e + x + 1]])
+        when "IY" # Indirect Indexed
+          e = mem.next
 
-        mem.int16([mem[e], mem[e+1]]) + y
-      else
-        raise NotImplementedError, mode.inspect
+          mem.int16([mem[e], mem[e+1]]) + y
+        else
+          raise NotImplementedError, mode.inspect
       end
     end
   end
